f7d8ca8300f045d2a8ed4150e6128a6ac0b8ffcf
Aggrathon <antonbjo@gmail.com>
2023-02-25 01:01:07 +0200

    shuffle test

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -35,6 +35,10 @@ impl<T: std::hash::Hash + std::cmp::Eq> Counter<T> {
         };
         self.0.insert(item, cnt);
     }
+
+    pub fn iter(&self) -> std::collections::hash_map::Iter<T, usize> {
+        self.0.iter()
+    }
 }
 
 impl<T: std::hash::Hash + std::cmp::Eq> Default for Counter<T> {
@@ -68,41 +72,40 @@ impl<T> TaggedShuffle<T> {
         if self.order.is_empty() {
             return;
         }
-        self.order.shuffle(&mut rand::thread_rng());
+        let mut prng = rand::thread_rng();
+        self.order.shuffle(&mut prng);
         let n = self.order.len();
         let lookahead = std::cmp::min(max_lookahead, n / self.max_same);
         // SAFETY: The size of the list is guaranteed by modulo
         unsafe {
             let mut sweep = |i: usize| {
                 let curr = *self.order.get_unchecked(i);
-                let after = i + lookahead;
-                let mut swp = 0;
+                let mut swp = i;
+                let mut hit = false;
                 for j in (i + 1)..(i + lookahead) {
                     let j = j % n;
                     let mut nex = *self.order.get_unchecked(j);
+                    swp += 1;
                     while curr == nex {
+                        hit = true;
                         swp += 1;
-                        self.order.swap(j, (swp + after) % n);
+                        self.order.swap(j, swp % n);
                         nex = *self.order.get_unchecked(j);
                     }
                 }
+                swp = if hit { swp - i } else { 0 };
                 swp
             };
-            let mut chain = 0;
+            let mut chain: usize = 0;
             for i in 0..n {
-                chain = std::cmp::max(chain, sweep(i));
+                chain = std::cmp::max(chain.saturating_sub(1), sweep(i));
             }
-            let mut count = 0;
-            for i in 0..(n - lookahead) {
-                let swp = sweep(i);
-                if swp == 0 {
-                    count += 1;
-                    if count >= chain {
-                        break;
+            'outer: for _ in 0..5 {
+                for i in 0..n {
+                    if chain < 1 {
+                        break 'outer;
                     }
-                } else {
-                    chain = std::cmp::max(chain, swp);
-                    count = 0;
+                    chain = std::cmp::max(chain - 1, sweep(i));
                 }
             }
         }
@@ -144,12 +147,17 @@ impl<T> TaggedShuffle<TaggedShuffle<T>> {
         self.shuffle(max_lookahead);
     }
 
-    pub fn nested_iter(&mut self) -> TaggedShuffleNestedIterator<T> {
+    pub fn nested_iter(&self) -> TaggedShuffleNestedIterator<T> {
         TaggedShuffleNestedIterator {
             shuffle: self,
             outer: 0,
             inner: std::iter::repeat(0).take(self.items.len()).collect(),
         }
+    }
+
+    pub fn nested_add(&mut self, item: TaggedShuffle<T>) {
+        let len = item.len();
+        self.addn(item, len);
     }
 }
 
@@ -179,7 +187,7 @@ impl<'a, T> Iterator for TaggedShuffleNestedIterator<'a, T> {
     fn next(&mut self) -> Option<Self::Item> {
         let i = *self.shuffle.order.get(self.outer)?;
         let j;
-        // SAFETY: self.inner.len() always matches self.shuffle.items_len()
+        // SAFETY: self.inner.len() always matches self.shuffle.items.len()
         unsafe {
             let inner = self.inner.get_unchecked_mut(i);
             j = *inner;
@@ -188,5 +196,54 @@ impl<'a, T> Iterator for TaggedShuffleNestedIterator<'a, T> {
         let out = self.shuffle.get(self.outer)?.get(j);
         self.outer += 1;
         out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_counter() {
+        let mut cnt = Counter::new();
+        cnt.add(0);
+        cnt.add(2);
+        cnt.addn(5, 10);
+        cnt.addn(0, 2);
+        let mut iter: Vec<_> = cnt.iter().map(|(a, b)| (*a, *b)).collect();
+        iter.sort();
+        assert_eq!(iter, vec![(0, 3), (2, 1), (5, 10)]);
+    }
+
+    #[test]
+    fn test_shuffle() {
+        for _ in 0..10 {
+            let mut ts = TaggedShuffle::new();
+            for i in 0..4 {
+                ts.addn(i, i + 1);
+            }
+            ts.shuffle(10);
+            assert!(ts.iter().zip(ts.iter().skip(1)).all(|(a, b)| a != b));
+        }
+    }
+
+    #[test]
+    fn test_nested_shuffle() {
+        let mut ts = TaggedShuffle::new();
+        for i in 0..4 {
+            let mut ts2 = TaggedShuffle::new();
+            ts2.addn(i, 4);
+            ts.nested_add(ts2);
+        }
+        ts.nested_shuffle(10);
+        // dbg!(ts.nested_iter().collect::<Vec<_>>());
+        assert!(ts
+            .iter()
+            .zip(ts.iter().skip(1))
+            .all(|(a, b)| !std::ptr::eq(a, b)));
+        assert!(ts
+            .nested_iter()
+            .zip(ts.nested_iter().skip(1))
+            .all(|(a, b)| a != b));
     }
 }
